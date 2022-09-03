cf581010e0f13c27a2982dcf384f6b3b5bc9d63b
Alperen deniz Zengin <azengin@k1m23s08.42istanbul.com.tr>
2022-09-03 14:05:54 +0300

    son

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -6,48 +6,40 @@
 /*   By: azengin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 06:59:50 by azengin           #+#    #+#             */
-/*   Updated: 2022/09/03 07:31:34 by azengin          ###   ########.fr       */
+/*   Updated: 2022/09/03 13:17:41 by azengin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
-
+/*#include <stdio.h>*/
 char	*ft_strcapitalize(char *str)
 {
 	int	i;
-	int	Uc;
+	int	uc;
 
-	Uc = 1;
-
+	i = 0;
+	uc = 1;
 	while (str[i] != '\0')
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			if (Uc == 1)
-			{
-			str[i] = str[i] - 32;
-			Uc = 0;
-			}
+			if (uc == 1)
+				str[i] = str[i] - 32;
+				uc = 0;
 		}
 		else if (str[i] >= '0' && str[i] <= '9')
-		{
-			Uc = 0;
-		}
-		else if (str[i] <= 'Z' && str[i] >= 'A' && Uc == 0)
-		{
+			uc = 0;
+		else if (str[i] <= 'Z' && str[i] >= 'A' && uc == 0)
 			str[i] = str[i] + 32;
-		}
 		else
-		{
-			Uc = 1;
+		{			
+			uc = 1;
 		}
 		i++;
 	}
 	return (str);
 }
-
-int	main(void)
+/* int	main(void)
 {
 	char	test[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
 
 	printf("%s", ft_strcapitalize(test));
-}
+}*/
--- c02/ex10/ft_strlcpy.c
+++ c02/ex10/ft_strlcpy.c
@@ -6,18 +6,19 @@
 /*   By: azengin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 07:39:52 by azengin           #+#    #+#             */
-/*   Updated: 2022/09/03 08:05:39 by azengin          ###   ########.fr       */
+/*   Updated: 2022/09/03 13:39:56 by azengin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdio.h>
 
-void	ft_strlen (char *str)
+int	ft_strlen (char *str)
 {
 	int	len;
 	
 	len = 0;
 	while (str[len] != '\0')
 	{
-		len++
+		len++;
 	}
 	return (len);
 }
@@ -32,12 +33,26 @@ unsigned int ft_strlcpy(char *dest, char *src, unsigned int size)
 
 	if (size != 0)
 	{
-		while (src[i] != '\0' && i < (dsize - 1))
+		while (src[i] != '\0' && i < (dsize))
 		{
-			dst[i] = src[i];
+			dest[i] = src[i];
 			i++;
 		}
-		dst[i] = '\0';
+		dest[i] = '\0';
 	}
 	return (size);
+}
+
+int	main(void)
+{
+	char	testsrc[] = "denizengin";
+	char	testdest[] = "test";
+
+	printf("%s\n", testsrc);
+	printf("%s\n", testdest);
+
+	ft_strlcpy(testdest, testsrc, 10);
+
+	printf("%s\n", testsrc);
+	printf("%s\n", testdest);
 }
