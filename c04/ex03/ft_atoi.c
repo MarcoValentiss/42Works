36686b51cfcec06b2c33b4627e88fa6c873e651b
Alperen deniz Zengin <azengin@k1m25s03.42istanbul.com.tr>
2022-09-10 15:43:44 +0300

    21

--- c04/ex03/ft_atoi.c
+++ c04/ex03/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: azengin <azengin@student.42istanbul.com.t  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 16:58:53 by azengin           #+#    #+#             */
-/*   Updated: 2022/09/09 17:45:37 by azengin          ###   ########.fr       */
+/*   Updated: 2022/09/10 15:42:33 by azengin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_whitespaces(char *str, int *pt_i)
 	count = 1;
 	while (str[i])
 	{
-		while (str[i] >= 9 && str[i] <= 13 || str[i] == 32)
+		while (str[i] >= 9 && (str[i] <= 13 || str[i] == 32))
 		{
 			i++;
 		}
@@ -33,16 +33,32 @@ int	ft_whitespaces(char *str, int *pt_i)
 		}
 	}
 	return (count);
+	*pt_i = i;
 }
 
 int	ft_atoi(char *str)
 {
 	int	i;
 	int	sign;
+	int result;
 	
+	result = 0;
 	sign = ft_whitespaces(str, &i);
 	while (str[i] && str[i] >= 48 && str[i] <= 57)
 	{
+		result *= 10;
+		result += str[i] - 48;
+		i++;
+	}
+	result *= sign;
+	return (result);
+}
 
+#include <stdio.h>
 
+int	main(void)
+{
+	char	atoitest[] = "     		 ++--++-12415ag241";
 
+	printf("%d", ft_atoi(atoitest));
+}
