610a044f6685376b4c49281d40cdc0ba85c38339
Deniz Zengin <denizzengin@Denizs-MacBook-Air.local>
2022-10-22 14:12:20 +0300

    1

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -1,6 +1,23 @@
 #include <stdlib.h>
+#include "libft.h"
 
 char    *ft_sttrrim(char const *s1, char const *set)
 {
+    int i;
+    int j;
+    char *p;
 
+    p = (char *)malloc(sizeof(char) * ft_strlen(s1));
+    i = 0;
+    j = 0;
+    while (s1[i])
+    {
+        if((ft_strchr(set, (int)s1[i])) = 0)
+        {
+            p[j] = s1[i];
+            j++;
+        }
+        i++;
+    }
+    return (p);
 }
